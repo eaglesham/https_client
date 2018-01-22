4bb8e789aa23f64c5adde05a753ab79af9255a8b
edward eaglesham <edward.eaglesham@gmail.com>
2018-01-22 17:27:58 +0000

    completed step 3: printing with parameters

---
+++ printing_withparameters.js
@@ -0,0 +1,29 @@
+var https = require('https');
+
+function getAndPrintHTML (options) {
+  options = requestOptions;
+
+  var dataBuffer = {};
+
+  https.get(options, function(response) {
+
+    response.setEncoding('utf8');
+
+    response.on('data', function(data){
+      console.log('Chunk received. Length:', data.length);
+      dataBuffer += data;
+    });
+
+    response.on('end', function() {
+      console.log(dataBuffer);
+    });
+
+  });
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step3.html'
+};
+
+getAndPrintHTML(requestOptions);
