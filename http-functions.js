6992c2cd59eda75d0a7ce9c84420083d2a5c4ad8
edward eaglesham <edward.eaglesham@gmail.com>
2018-01-22 18:22:09 +0000

    exported http functions as module. set up js file to test module

---
+++ http-functions.js
@@ -0,0 +1,36 @@
+var https = require('https');
+
+module.exports = function getHTML (options, callback) {
+
+  function getHTML (options, callback) {
+
+    options = requestOptions;
+
+    var dataBuffer = {};
+
+    https.get(options, function(response) {
+
+      response.setEncoding('utf8');
+
+      response.on('data', function(data) {
+        console.log('Chunk received. Length:', data.length);
+        dataBuffer += data;
+      });
+
+      response.on('end', function() {
+        callback(dataBuffer);
+      });
+    });
+  };
+
+  function printHTML (html) {
+    console.log(html);
+  }
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step4.html'
+  };
+
+  getHTML(requestOptions, printHTML);
+}
